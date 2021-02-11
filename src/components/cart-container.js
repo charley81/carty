db06572daac64e0425fc6b98873177a641c7953f
ChrisKnowsDev <chrisharley81@gmail.com>
2021-02-11 15:33:35 -0500

    header/nav ui complete

--- src/components/cart-container.js
+++ src/components/cart-container.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import CartItem from './cart-item'
+import { useGlobalContext } from '../context'
 
 const CartContainer = () => {
+  const { cart, total } = useGlobalContext()
   return <div>cart container</div>
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,67 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react'
+import { css } from '@emotion/react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { useGlobalContext } from '../context'
 
 const Navbar = () => {
   const { amount } = useGlobalContext()
-  console.log(amount)
-  return <div>navbar</div>
+
+  return (
+    <header
+      css={css`
+        background-color: var(--primaryColor);
+        color: var(--lightColor);
+        padding: 1rem;
+
+        span {
+          color: var(--secondaryColor);
+          text-shadow: 0.5px 0.5px 2px rgba(255, 255, 255, 0.7);
+        }
+
+        h1 {
+          color: var(--lightColor);
+        }
+
+        .header-container {
+          max-width: var(--maxWidth);
+          display: flex;
+          justify-content: space-between;
+          margin: auto;
+        }
+
+        .cart-amount {
+          display: flex;
+          align-items: center;
+          position: relative;
+        }
+
+        .icon {
+          font-size: 3rem;
+        }
+
+        p {
+          font-size: 1.1rem;
+          background: var(--secondaryColor);
+          position: absolute;
+          top: -10px;
+          left: 90%;
+          padding: 0.25rem;
+          border-radius: var(--borderRadius);
+        }
+      `}
+    >
+      <div className="header-container">
+        <h1>
+          cart<span>y</span>
+        </h1>
+        <div className="cart-amount">
+          <AiOutlineShoppingCart className="icon" />
+          <p>{amount}</p>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
