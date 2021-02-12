ab14c5ab9bfd406b1f910939cf9464d7ae878d9f
ChrisKnowsDev <chrisharley81@gmail.com>
2021-02-11 19:54:01 -0500

    completed cartItems ui

--- src/components/cart-item.js
+++ src/components/cart-item.js
@@ -1,7 +1,40 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react'
+import { css } from '@emotion/react'
+import { BsArrowUp, BsArrowDown } from 'react-icons/bs'
 
 const CartItem = ({ id, img, title, price, amount }) => {
-  return <div>cart item</div>
+  return (
+    <article
+      css={css`
+        img {
+          width: 100px;
+          height: 100px;
+        }
+      `}
+    >
+      {/* image */}
+      <img src={img} alt={title} />
+
+      {/* phone info */}
+      <div className="info">
+        <h4 className="title">{title}</h4>
+        <h4 className="price">{price}</h4>
+        <button className="remove">remove</button>
+      </div>
+
+      {/* increase/decrease buttons */}
+      <div className="btn-container">
+        <button className="increase">
+          <BsArrowUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="decrease">
+          <BsArrowDown />
+        </button>
+      </div>
+    </article>
+  )
 }
 
 export default CartItem
