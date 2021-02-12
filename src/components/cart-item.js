f71a234a81f5975eee4d229872c952013ff1ccbb
ChrisKnowsDev <chrisharley81@gmail.com>
2021-02-12 15:46:16 -0500

    remove item functionality complete

--- src/components/cart-item.js
+++ src/components/cart-item.js
@@ -2,8 +2,11 @@
 import React from 'react'
 import { css } from '@emotion/react'
 import { BsArrowUp, BsArrowDown } from 'react-icons/bs'
+import { useGlobalContext } from '../context.js'
 
 const CartItem = ({ id, img, title, price, amount }) => {
+  const { removeItem } = useGlobalContext()
+
   return (
     <article
       css={css`
@@ -31,6 +34,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
           padding: 0.25rem 0.5rem;
           border-radius: var(--borderRadius);
           font-family: var(--fontFamily);
+          cursor: pointer;
         }
 
         .btn-container {
@@ -50,7 +54,9 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <div className="info">
         <h4 className="title">{title}</h4>
         <h4 className="price">{price}</h4>
-        <button className="remove">remove</button>
+        <button className="remove" onClick={() => removeItem(id)}>
+          remove
+        </button>
       </div>
 
       {/* increase/decrease buttons */}
--- src/context.js
+++ src/context.js
@@ -15,9 +15,10 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const clearCart = () => dispatch({ type: 'CLEAR_CART' })
+  const removeItem = id => dispatch({ type: 'REMOVE_ITEM', payload: id })
 
   return (
-    <AppContext.Provider value={{ ...state, clearCart }}>
+    <AppContext.Provider value={{ ...state, clearCart, removeItem }}>
       {children}
     </AppContext.Provider>
   )
